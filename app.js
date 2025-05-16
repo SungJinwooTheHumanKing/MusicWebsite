ff59397f519bc202063baa297d05a196d7de7010
SungJinwooTheHumanKing <arpeetsharma371@gmail.com>
2025-05-16 21:47:04 +0530

    Update app.js

--- app.js
+++ app.js
@@ -6,3 +6,16 @@ document.querySelectorAll('audio').forEach(audio => {
     });
   });
 });
+
+const playlistItems = document.querySelectorAll('.track');
+const audio = document.getElementById('main-audio');
+
+playlistItems.forEach(item => {
+  item.addEventListener('click', () => {
+    const src = item.getAttribute('data-src');
+    audio.src = src;
+    audio.play();
+    playlistItems.forEach(el => el.classList.remove('bg-purple-700'));
+    item.classList.add('bg-purple-700');
+  });
+});
